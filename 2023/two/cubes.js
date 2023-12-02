0f13fc6a31bef2db8b7e50ad917237f0c9752d6b
Simon Lauer <lauersimon2006@gmail.com>
2023-12-02 19:10:35 +0100

    part one

--- 2023/two/cubes.js
+++ 2023/two/cubes.js
@@ -1,3 +1,45 @@
 import { testData } from "./testData.js";
 
-console.log("ayoo");
+const maxCubes = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const allColors = ["red", "green", "blue"];
+
+const getColors = (val) => {
+  const colors = allColors.reduce(
+    (acc, color) => {
+      const regexp = new RegExp(`\\d+(?: ${color})`, "g");
+
+      acc[color] = val
+        .match(regexp)
+        .map((match) => match.replace(/\D/g, "").trim());
+
+      return acc;
+    },
+    { red: [], green: [], blue: [] }
+  );
+
+  return colors;
+};
+
+const isAbove = (color, cubesByColor) =>
+  cubesByColor.some((cubes) => cubes > maxCubes[color]);
+
+const yeet = testData.reduce((acc, val, index) => {
+  const colors = getColors(val);
+
+  if (
+    isAbove("red", colors.red) ||
+    isAbove("blue", colors.blue) ||
+    isAbove("green", colors.green)
+  ) {
+    return acc;
+  }
+
+  return acc + index + 1;
+}, 0);
+
+console.log(yeet);
